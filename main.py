6fd7285ac95db3a0c95c4cc726efcfc2e41784be
grysha11 <grzhzahar@gmail.com>
2025-08-21 22:53:22 +0200

    functions are actually calling

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.write_file import schema_write_file
-from functions.run_python import schema_run_python_file
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.write_file import schema_write_file, write_file
+from functions.run_python import schema_run_python_file, run_python_file
 
 system_prompt = """
 You are a helpful AI coding agent.
@@ -29,6 +29,13 @@ available_functions = types.Tool(
         schema_run_python_file,
     ]
 )
+
+function_names = {
+    "get_files_info" : get_files_info,
+    "get_file_content" : get_file_content,
+    "write_file" : write_file,
+    "run_python_file" : run_python_file,
+}
 
 def main():
     if len(sys.argv) < 2:
@@ -66,13 +73,44 @@ def generate_response(client, messages, verbose):
     if response.function_calls:
         function_calls = response.function_calls
         for function_call_part in function_calls:
-            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+            res = call_function(function_call_part, verbose)
+            if not res.parts[0].function_response.response:
+                raise RuntimeError("Fatal: no respose on function call.")
+            if verbose:
+                print(f"-> {res.parts[0].function_response.response}")
     else:
         print(response.text)
     if verbose:
         print(f"User prompt: {messages}")
         print(f"Prompt tokens: {metadata.prompt_token_count}")
         print(f"Response tokens: {metadata.candidates_token_count}")
+
+def call_function(function_call_part, verbose=False):
+    if function_call_part.name not in function_names:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_call_part.name,
+                    response={"error": f"Unknown function: {function_call_part.name}"},
+                )
+            ],
+        )
+    args = dict(function_call_part.args)
+    args["working_directory"] = "calculator"
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    print(f" - Calling function: {function_call_part.name}")
+    result = function_names[function_call_part.name](**args)
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_call_part.name,
+                response={"result": result},
+            )
+        ],
+    )
 
 if __name__ == "__main__":
     main()
